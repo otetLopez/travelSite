ea087d74fb640bae0bb1c5c1a2b30530bee209c8
otetLopez <otet.tud@gmail.com>
2020-03-17 03:37:52 -0400

    Add register form

--- script/log.js
+++ script/log.js
@@ -1,8 +1,11 @@
 var modal = document.getElementById('id01');
+var modalR = document.getElementById('id02');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target == modalR) {
         modal.style.display = "none";
+    }  else if (event.target == modalR) {
+        modalR.style.display = "none";
     }
 }
