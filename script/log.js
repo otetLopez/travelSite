c155c8849c88e54b2eab15e1360f2ba8ea827533
otetLopez <otet.tud@gmail.com>
2020-03-19 05:18:12 -0400

    Fix register issues

--- script/log.js
+++ script/log.js
@@ -1,95 +1,94 @@
 var modal = document.getElementById('id01');
 var modalR = document.getElementById('id02');
-
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modalR) {
-        modal.style.display = "none";
-    }  else if (event.target == modalR) {
-        modalR.style.display = "none";
-    }
-}
+// window.onclick = function(event) {
+//     if (event.target == modalR) {
+//         modal.style.display = "none";
+//     }  else if (event.target == modalR) {
+//         modalR.style.display = "none";
+//     }
+// };
 
 $(document).ready(function () {
-  console.log("Entering Query");
+  console.log("Register");
     var i = 0;
-    for (i = 0; i <= localStorage.length; i++) {
-        var reqID = "req-" + i;
-        if(localStorage.getItem(reqID) !== null) {
-          $('#requestList').append("<li id='" + reqID + "'>" + localStorage.getItem(reqID) + "</li>");
+    var usrIdx = 0;
+    // retrieveUsers();
+    for (usrIdx = 0; usrIdx <= localStorage.length; usrIdx++) {
+      var usrID = "usr-" + usrIdx;
+      var currUsr = localStorage.getItem(usrID);
+      if(currUsr !== null) {
+        console.log("Retrieved user: " + currUsr);
       }
     }
-    $('#clear').click(function () {
-        localStorage.clear();
-    });
-    $('#book_form').submit(function () {
-      console.log("Submitting details");
-        if ($('#taskInput').val() !== "") {
-            var taskID = "task-" + i;
-            var taskMessage = $('#taskInput').val();
-            localStorage.setItem(taskID, taskMessage);
-            $('#taskList').append("<li class='task' id='" + taskID + "'>" + taskMessage + "</li>");
-            var task = $('#' + taskID);
-            task.css('display', 'none');
-            task.slideDown();
-            $('#taskInput').val("");
-            i++;
-        }
-        return false;
-    });
-
-    $('#requestList').on("click", "li", function (event) {
-        self = $(this);
-        reqID = self.attr('id');
-        if(confirm("Are you sure you want to remove " + localStorage.getItem(reqID) + "?")) {
-          localStorage.removeItem(reqID);
-          self.slideUp('slow', function () {
-              self.remove();
-          });
-        }
-    });
 
     $("button").click(function(){
-      var id = $(this).attr('id');
-      alert(id)
-    //   var reqID = "";
-    //   var checkIn = "";
-    //   var checkOut = "";
-    //   var guests = "";
-    //   var room = "";
-    //   reqID = "req-" + i;
-    //
-    //   if(id.localeCompare("Shangri-La Hotel") === 0) {
-    //     checkIn = $('#cIn0').val();
-    //     checkOut = $('#cOut0').val();
-    //     guests = $('#guests0').val();
-    //     room = $('#room0').val();
-    //   } else if(id.localeCompare("JPark Island Resort and Waterpark") === 0) {
-    //     checkIn = $('#cIn1').val();
-    //     checkOut = $('#cOut1').val();
-    //     guests = $('#guests1').val();
-    //     room = $('#room1').val();
-    //   } else if(id.localeCompare("Plantation Bay Resort and Spa") === 0) {
-    //     checkIn = $('#cIn2').val();
-    //     checkOut = $('#cOut2').val();
-    //     guests = $('#guests2').val();
-    //     room = $('#room2').val();
-    //   }
-    //
-    //   if(checkIn !== "" && checkOut !== "" && guests !== "") {
-    //     var fullRequest = id + ": " + "Check-In Date: " + checkIn + ", Check-Out Date: " + checkOut;
-    //     fullRequest = fullRequest + ", Guest: " + guests + ", Room Type: " + room;
-    //     console.log("Requesting --> " + fullRequest);
-    //
-    //     localStorage.setItem(reqID, fullRequest);
-    //     $('#requestList').append("<li class='request' id='" + reqID + "'>" + fullRequest + "</li>");
-    //     var req = $('#' + reqID);
-    //     req.css('display', 'none');
-    //     req.slideDown();
-    //     console.log("Total stored: " + localStorage.length);
-    //     i++;
-    //   } else {
-    //     alert("Cannot Book Request.  Please complete all form fields")
-    //   }
-    // });
+      console.log("Button Clicked");
+      if($(this).attr('id') !== undefined) {
+        var id = $(this).attr('id');
+        console.log("This is the ID : " + id);
+        if(id.localeCompare("log01") === 0) {
+          console.log("Logging In");
+        } else if (id.localeCompare("log02") === 0) {
+          console.log("User Registering");
+          var uname =  $('#uname').val();
+          var pwd =  $('#psw1').val();
+          var cpwd =  $('#psw2').val();
+          if(uname !== "" && pwd !== "" && cpwd !== "") {
+            console.log(uname + ", " + pwd + ", " + cpwd);
+            if(pwd.localeCompare(cpwd) !== 0) {
+              alert("Passwords entered do not match!")
+            } else if (checkUser(uname) === false) { //Should not do this here but i am so sleepy
+              alert("Username already exist!  Try another username.")
+            } else if (pwd.length < 6){
+              alert("Sorry!  Your password must contain  a capital letter, two numbers, a symbol, an inspiring message, a spell, a gang sign, a heiroglyph and a blood of a virgin.")
+            } else {
+              var usrID = "usr-" + usrIdx;
+              var pswID = "psw-" + usrIdx;
+              localStorage.setItem(usrID, uname);
+              localStorage.setItem(pswID, pwd);
+              console.log("Total stored: " + localStorage.length);
+              alert("Register Successful!  Please Log In.");
+              usrIdx++;
+              clearFields();
+              dismissForm();
+            }
+          }
+        }
+      }
+    });
 });
+
+function checkUser(uname) {
+
+  // Displaying all registered users
+  for (i = 0; i <= localStorage.length; i++) {
+      var usrID = "usr-" + i;
+      var currUsr = localStorage.getItem(usrID);
+      if(currUsr !== null) {
+        console.log("Retrieved user: " + currUsr);
+      }
+  }
+
+  for (i = 0; i <= localStorage.length; i++) {
+      var usrID = "usr-" + i;
+      var currUsr = localStorage.getItem(usrID);
+      if(currUsr !== null) {
+        if(currUsr.localeCompare(uname) === 0) {
+          return false;
+        }
+      }
+  }
+  return true;
+}
+
+function clearFields() {
+  $('#uname').val("");
+  $('#psw1').val("");
+  $('#psw2').val("");
+}
+
+function dismissForm() {
+  document.getElementById("id01").style.display = 'none';
+  document.getElementById("id02").style.display = 'none';
+}
--- script/main.js
+++ script/main.js
@@ -1,16 +1,12 @@
 window.onscroll = function() {myFunction()};
 
 var header = document.getElementById("navcontainer");
-console.log("Header-->" + header.offsetTop);
 var sticky = header.offsetTop;
 
 function myFunction() {
-  console.log("Scrolling with offset " + window.pageYOffset);
   if (window.pageYOffset > sticky) {
-    console.log("Adding");
     header.classList.add("sticky");
   } else {
-    console.log("Removing");
     header.classList.remove("sticky");
   }
 }
