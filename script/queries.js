a1c1b3ec3d0d86fb9bbdb13b97972074bd62210d
otetLopez <otet.tud@gmail.com>
2020-03-19 22:51:29 -0400

    Clear list when user changes

--- script/queries.js
+++ script/queries.js
@@ -1,32 +1,18 @@
 $(document).ready(function () {
   console.log("Entering Query");
+    let loggedUser = "undefined";
     var i = 0;
-    for (i = 0; i <= localStorage.length; i++) {
-        var reqID = "req-" + i;
-        if(localStorage.getItem(reqID) !== null) {
-          $('#requestList').append("<li id='" + reqID + "'>" + localStorage.getItem(reqID) + "</li>");
-      }
-    }
+
+    // Let us find out if a user is currently logged on
+    var element = document.getElementById("instruct");
+    retrievedLoggedUser();
+
     $('#clear').click(function () {
         localStorage.clear();
     });
-    $('#book_form').submit(function () {
-      console.log("Submitting details");
-        if ($('#taskInput').val() !== "") {
-            var taskID = "task-" + i;
-            var taskMessage = $('#taskInput').val();
-            localStorage.setItem(taskID, taskMessage);
-            $('#taskList').append("<li class='task' id='" + taskID + "'>" + taskMessage + "</li>");
-            var task = $('#' + taskID);
-            task.css('display', 'none');
-            task.slideDown();
-            $('#taskInput').val("");
-            i++;
-        }
-        return false;
-    });
 
     $('#requestList').on("click", "li", function (event) {
+      retrievedLoggedUser();
         self = $(this);
         reqID = self.attr('id');
         if(confirm("Are you sure you want to remove " + localStorage.getItem(reqID) + "?")) {
@@ -38,13 +24,14 @@ $(document).ready(function () {
     });
 
     $("button").click(function(){
+      retrievedLoggedUser();
       var id = $(this).attr('id');
       var reqID = "";
       var checkIn = "";
       var checkOut = "";
       var guests = "";
       var room = "";
-      reqID = "req-" + i;
+      reqID = "req-" + loggedUser + "-" +i;
       var isBook = false;
 
       if(id.localeCompare("Shangri-La Hotel") === 0) {
@@ -67,28 +54,63 @@ $(document).ready(function () {
         isBook = true;
       } else {
         isBook = false;
+        console.log("[QUERIES] Book should be false");
       }
+      console.log("[QUERIES] Logged user is: " + loggedUser);
+      if(loggedUser !== null && loggedUser.localeCompare("undefined") !== 0) {
+        if(checkIn !== "" && checkOut !== "" && guests !== "" && isBook === true) {
+          var fullRequest = id + ": " + "Check-In Date: " + checkIn + ", Check-Out Date: " + checkOut;
+          fullRequest = fullRequest + ", Guest: " + guests + ", Room Type: " + room;
+          console.log("Requesting --> " + reqID + ":" + fullRequest);
 
-      if(checkIn !== "" && checkOut !== "" && guests !== "" && isBook === true) {
-        var fullRequest = id + ": " + "Check-In Date: " + checkIn + ", Check-Out Date: " + checkOut;
-        fullRequest = fullRequest + ", Guest: " + guests + ", Room Type: " + room;
-        console.log("Requesting --> " + fullRequest);
-
-        localStorage.setItem(reqID, fullRequest);
-        $('#requestList').append("<li class='request' id='" + reqID + "'>" + fullRequest + "</li>");
-        var req = $('#' + reqID);
-        req.css('display', 'none');
-        req.slideDown();
-        console.log("Total stored: " + localStorage.length);
-        clearFields();
-        i++;
+          localStorage.setItem(reqID, fullRequest);
+          $('#requestList').append("<li class='request' id='" + reqID + "'>" + fullRequest + "</li>");
+          var req = $('#' + reqID);
+          req.css('display', 'none');
+          req.slideDown();
+          console.log("Total stored: " + localStorage.length);
+          clearFields();
+          i++;
+        } else {
+          if(isBook === true) {
+            alert("Cannot Book Request.  Please complete all form fields");
+          }
+        }
       } else {
         if(isBook === true) {
-          alert("Cannot Book Request.  Please complete all form fields");
+          alert("Cannot Book Request.  Please Log In to your account or Register");
         }
       }
     });
+
+    function retrievedLoggedUser() {
+      document.getElementById("requestList").innerHTML = "";
+      loggedUser = localStorage.getItem("currUser");
+      console.log("[QUERIES] Logged user retrieved: " + loggedUser);
+      element = document.getElementById("instruct");
+      if(typeof(element) != 'undefined' && element != null){
+        if(loggedUser !== null && (loggedUser.localeCompare("undefined") !== 0)) {
+          console.log("Retrieving requests fo user: " + loggedUser);
+          var requestFlag = false;
+          for (i = 0; i <= localStorage.length; i++) {
+              var reqID = "req-" + loggedUser + "-" +i;
+              if(localStorage.getItem(reqID) !== null) {
+                $('#requestList').append("<li id='" + reqID + "'>" + localStorage.getItem(reqID) + "</li>");
+                requestFlag = true;
+            }
+          }
+          if(requestFlag === true) {
+            document.getElementById("instruct").innerHTML = "Click on item to remove from list";
+          } else {
+            document.getElementById("instruct").innerHTML = loggedUser + ", you have no saved requests.";
+          }
+        } else {
+          document.getElementById("instruct").innerHTML = "Log In to view your requests."
+        }
+      }
+    }
 });
+
 
 function clearFields() {
   $('#cIn0').val("");
